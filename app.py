3cd675e21ae4efbd3475dd5459faf7b71f83b17b
Malindu Mahara <59385501+malindu20@users.noreply.github.com>
2024-04-25 20:41:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-  return 'Hello from Mahara ECS Container. 3rd Time'
+  return 'Hello from Mahara ECS Container. 4th Time'
 
 app.run(host='0.0.0.0')
